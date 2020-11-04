19dae50282bd8b05c5d0ff0cb0008d89361d8aef
rfeinman <rfeinman16@gmail.com>
2020-11-04 17:44:51 -0500

    initial prototype of full seq2seq model

---
+++ sketch_rnn/mix_layer.py
@@ -0,0 +1,40 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MixLayer(nn.Module):
+    def __init__(self, input_size, k, d=2, reg_cov=0.):
+        super().__init__()
+        self.linear = nn.Linear(input_size, k + 2*k*d + k + 3)
+        self.k = k
+        self.d = d
+        self.reg_cov = reg_cov
+
+    def forward(self, x, T=1):
+        x = self.linear(x)
+
+        # unpack variables: [mix_probs, means, scales, corrs, endings]
+        d, k = self.d, self.k
+        splits = [k, k*d, k*d, k, 3]
+        mix_logits, means, scales, corrs, v_logits = x.split(splits, -1)
+        mix_logp = F.log_softmax(mix_logits, -1) # [n,t,k]
+        scales = torch.exp(scales) # [n,t,k,d]
+        corrs = torch.tanh(corrs) # [n,t,k]
+        v_logp = F.log_softmax(v_logits, -1) # [n,t,3]
+
+        # reshape [...,k*d] -> [...,k,d]
+        means = means.reshape(*means.shape[:-1], -1, 2)
+        scales = scales.reshape(*scales.shape[:-1], -1, 2)
+
+        # add tikhonov regularization
+        if self.reg_cov > 0:
+            raise NotImplementedError
+
+        if T != 1:
+            v_logp = F.log_softmax(v_logp/T, -1)
+            mix_logp = F.log_softmax(mix_logp/T, -1)
+            scales = scales * math.sqrt(T)
+
+        return mix_logp, means, scales, corrs, v_logp
---
+++ sketch_rnn/model.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torch.nn.utils.rnn as rnn_utils
+
+from .rnn import _cell_types
+from .mix_layer import MixLayer
+
+
+
+def pack_sequence(seq, lengths):
+    pack = rnn_utils.pack_padded_sequence(
+        seq, lengths, batch_first=True, enforce_sorted=False)
+    return pack
+
+class Encoder(nn.Module):
+    def __init__(self, hidden_size, z_size):
+        super().__init__()
+        self.rnn = nn.LSTM(5, hidden_size, bidirectional=True, batch_first=True)
+        self.output = nn.Linear(2*hidden_size, 2*z_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.rnn.reset_parameters()
+        nn.init.normal_(self.output.weight, 0., 0.001)
+        nn.init.zeros_(self.output.bias)
+
+    def forward(self, x, lengths):
+        x = pack_sequence(x, lengths)
+        _, (x, _) = self.rnn(x) # [2,batch,hid]
+        x = x.permute(1,0,2).flatten(1).contiguous() # [batch,2*hid]
+        z_mean, z_logvar = self.output(x).chunk(2, 1)
+        z = z_mean + torch.exp(0.5*z_logvar) * torch.randn_like(z_logvar)
+        return z, z_mean, z_logvar
+
+
+class Model(nn.Module):
+    def __init__(self, hps):
+        super().__init__()
+        # Decoder LSTM
+        assert hps.dec_model in ['lstm', 'layer_norm', 'hyper']
+        cell_init = _cell_types[hps.dec_model]
+        self.cell = cell_init(5, hps.dec_rnn_size, r_dropout=hps.r_dropout)
+        self.encoder = Encoder(hps.enc_rnn_size, hps.z_size)
+        self.state_init = nn.Sequential(hps.z_size, self.cell.state_size)
+        self.mix_layer = MixLayer(hps.dec_rnn_size, k=hps.num_mixture)
+        self.max_len = hps.max_seq_len
+        self.hps = hps
+
+    def forward(self, data, lengths):
+        # The target/expected vectors of strokes
+        output_x = data[:,1:self.max_len+1,:]
+        # vectors of strokes to be fed to decoder (include dummy value)
+        input_x = data[:,:self.max_len,:]
+
+        # encoder forward
+        z, z_mean, z_logvar = self.encoder(output_x, lengths)
+
+        # initialize decoder state
+        state = self.state_init(z).chunk(2, -1)
+
+        # decoder forward
+        input_x = input_x.unbind(1)
+        output = []
+        for t in range(self.max_len):
+            inputs = torch.cat((input_x[t], z), -1)
+            out, state = self.cell(inputs, state)
+            output.append(out)
+        output = torch.stack(output, 1) # [batch,steps,dim]
+
+        # mixlayer outputs
+        params = self.mix_layer(output)
+
+        return params
