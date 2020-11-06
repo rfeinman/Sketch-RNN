bcc59433324c99c1f08bcae553cd4736b3d0daaa
rfeinman <rfeinman16@gmail.com>
2020-11-06 12:09:51 -0500

    adding new AverageMeter class to utils

--- sketch_rnn/utils.py
+++ sketch_rnn/utils.py
@@ -1,21 +1,6 @@
-# Copyright 2020 The Magenta Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Lint as: python3
-"""SketchRNN data loading and image manipulation utilities."""
-import warnings
-import random
+"""
+SketchRNN data loading and image manipulation utilities.
+"""
 import numpy as np
 
 
@@ -198,3 +183,24 @@ class HParams():
         # data augmentation params
         self.random_scale_factor = 0.15
         self.augment_stroke_prob = 0.10
+
+
+class AverageMeter:
+    """Computes and stores the average and current value."""
+    def __init__(self):
+        self.val = 0.
+        self.avg = 0.
+        self.sum = 0.
+        self.count = 0.
+
+    def reset(self):
+        self.val = 0.
+        self.avg = 0.
+        self.sum = 0.
+        self.count = 0.
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
