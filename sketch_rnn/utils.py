8e737905004883d80c1f41fb6434dd5e93a27657
rfeinman <rfeinman16@gmail.com>
2020-11-06 08:15:12 -0500

    remove old (unused) DataLoader class, add HParams class

--- sketch_rnn/utils.py
+++ sketch_rnn/utils.py
@@ -194,136 +194,31 @@ def get_max_len(strokes):
     return max_len
 
 
-class DataLoader:
-    """UNUSED - see replacement in dataset.py"""
-    def __init__(self,
-                 strokes,
-                 batch_size=100,
-                 max_seq_length=250,
-                 scale_factor=1.0,
-                 random_scale_factor=0.0,
-                 augment_stroke_prob=0.0,
-                 limit=1000):
-        warnings.warn('Using now-defunct DataLoader class.')
-        self.batch_size = batch_size  # minibatch size
-        self.max_seq_length = max_seq_length  # N_max in sketch-rnn paper
-        self.scale_factor = scale_factor  # divide offsets by this factor
-        self.random_scale_factor = random_scale_factor  # data augmentation method
-        # Removes large gaps in the data. x and y offsets are clamped to have
-        # absolute value no greater than this limit.
-        self.limit = limit
-        self.augment_stroke_prob = augment_stroke_prob  # data augmentation method
-        self.start_stroke_token = [0, 0, 1, 0, 0]  # S_0 in sketch-rnn paper
-        # sets self.strokes (list of ndarrays, one per sketch, in stroke-3 format,
-        # sorted by size)
-        self.preprocess(strokes)
-
-    def preprocess(self, strokes):
-        """Remove entries from strokes having > max_seq_length points."""
-        raw_data = []
-        seq_len = []
-        count_data = 0
-
-        for i in range(len(strokes)):
-            data = strokes[i]
-            if len(data) <= (self.max_seq_length):
-                count_data += 1
-                # removes large gaps from the data
-                data = np.minimum(data, self.limit)
-                data = np.maximum(data, -self.limit)
-                data = np.array(data, dtype=np.float32)
-                data[:, 0:2] /= self.scale_factor
-                raw_data.append(data)
-                seq_len.append(len(data))
-        seq_len = np.array(seq_len)  # nstrokes for each sketch
-        idx = np.argsort(seq_len)
-        self.strokes = []
-        for i in range(len(seq_len)):
-            self.strokes.append(raw_data[idx[i]])
-        print("total images <= max_seq_len is %d" % count_data)
-        self.num_batches = int(count_data / self.batch_size)
-
-    def random_sample(self):
-        """Return a random sample, in stroke-3 format as used by draw_strokes."""
-        sample = np.copy(random.choice(self.strokes))
-        return sample
-
-    def random_scale(self, data):
-        """Augment data by stretching x and y axis randomly [1-e, 1+e]."""
-        x_scale_factor = (
-            np.random.random() - 0.5) * 2 * self.random_scale_factor + 1.0
-        y_scale_factor = (
-            np.random.random() - 0.5) * 2 * self.random_scale_factor + 1.0
-        result = np.copy(data)
-        result[:, 0] *= x_scale_factor
-        result[:, 1] *= y_scale_factor
-        return result
-
-    def calculate_normalizing_scale_factor(self):
-        """Calculate the normalizing factor explained in appendix of sketch-rnn."""
-        data = []
-        for i in range(len(self.strokes)):
-            if len(self.strokes[i]) > self.max_seq_length:
-                continue
-            for j in range(len(self.strokes[i])):
-                data.append(self.strokes[i][j, 0])
-                data.append(self.strokes[i][j, 1])
-        data = np.array(data)
-        return np.std(data)
-
-    def normalize(self, scale_factor=None):
-        """Normalize entire dataset (delta_x, delta_y) by the scaling factor."""
-        if scale_factor is None:
-            scale_factor = self.calculate_normalizing_scale_factor()
-        self.scale_factor = scale_factor
-        for i in range(len(self.strokes)):
-            self.strokes[i][:, 0:2] /= self.scale_factor
-
-    def _get_batch_from_indices(self, indices):
-        """Given a list of indices, return the potentially augmented batch."""
-        x_batch = []
-        seq_len = []
-        for idx in range(len(indices)):
-            i = indices[idx]
-            data = self.random_scale(self.strokes[i])
-            data_copy = np.copy(data)
-            if self.augment_stroke_prob > 0:
-                data_copy = augment_strokes(data_copy, self.augment_stroke_prob)
-            x_batch.append(data_copy)
-            length = len(data_copy)
-            seq_len.append(length)
-        seq_len = np.array(seq_len, dtype=int)
-        # We return three things: stroke-3 format, stroke-5 format, list of seq_len.
-        return x_batch, self.pad_batch(x_batch, self.max_seq_length), seq_len
-
-    def random_batch(self):
-        """Return a randomised portion of the training data."""
-        idx = np.random.permutation(len(self.strokes))[0:self.batch_size]
-        return self._get_batch_from_indices(idx)
-
-    def get_batch(self, idx):
-        """Get the idx'th batch from the dataset."""
-        assert idx >= 0, "idx must be non negative"
-        assert idx < self.num_batches, "idx must be less than the number of batches"
-        start_idx = idx * self.batch_size
-        indices = list(range(start_idx, start_idx + self.batch_size))
-        return self._get_batch_from_indices(indices)
-
-    def pad_batch(self, batch, max_len):
-        """Pad the batch to be stroke-5 bigger format as described in paper."""
-        result = np.zeros((self.batch_size, max_len + 1, 5), dtype=float)
-        assert len(batch) == self.batch_size
-        for i in range(self.batch_size):
-            l = len(batch[i])
-            assert l <= max_len
-            result[i, 0:l, 0:2] = batch[i][:, 0:2]
-            result[i, 0:l, 3] = batch[i][:, 2]
-            result[i, 0:l, 2] = 1 - result[i, 0:l, 3]
-            result[i, l:, 4] = 1
-            # put in the first token, as described in sketch-rnn methodology
-            result[i, 1:, :] = result[i, :-1, :]
-            result[i, 0, :] = 0
-            result[i, 0, 2] = self.start_stroke_token[2]  # setting S_0 from paper.
-            result[i, 0, 3] = self.start_stroke_token[3]
-            result[i, 0, 4] = self.start_stroke_token[4]
-        return result
+class HParams():
+    def __init__(self):
+        # dataset
+        self.data_set = ['cat.npz']
+        # model params
+        self.max_seq_len = 120
+        self.enc_model = 'lstm'
+        self.dec_model = 'layer_norm'
+        self.enc_rnn_size = 256
+        self.dec_rnn_size = 512
+        self.z_size = 128
+        self.num_mixture = 20
+        self.r_dropout = 0.1
+        self.batch_size = 100
+        # loss params
+        self.kl_weight = 0.5
+        self.kl_weight_start = 0.01 # eta_min
+        self.kl_tolerance = 0.2 # kl_min
+        self.kl_decay_rate = 0.99995 # R
+        # training params
+        self.lr = 0.001
+        self.lr_decay = 0.9999
+        self.min_lr = 0.00001
+        self.grad_clip = 1.
+        # data params
+        self.output_dropout_prob = 0.90  # Probability of output dropout keep.
+        self.random_scale_factor = 0.15  # Random scaling data augmentation proportion.
+        self.augment_stroke_prob = 0.10
