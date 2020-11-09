b1c6942e072101c97b82321edcce3adc3d583fc2
rfeinman <rfeinman16@gmail.com>
2020-11-08 20:29:53 -0500

    store z_size as instance variable for model

--- sketch_rnn/model.py
+++ sketch_rnn/model.py
@@ -58,6 +58,7 @@ class SketchRNN(nn.Module):
             kl_min=hps.kl_tolerance)
         self.loss_draw = DrawingLoss(reg_covar=hps.reg_covar)
         self.max_len = hps.max_seq_len
+        self.z_size = hps.z_size
         self.reset_parameters()
 
     def reset_parameters(self):
