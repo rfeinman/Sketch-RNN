43895b50a0a9f6ad23a6489f12faf6e755b80b49
rfeinman <rfeinman16@gmail.com>
2020-11-10 13:58:43 -0500

    minor refactor of param_layer

--- sketch_rnn/param_layer.py
+++ sketch_rnn/param_layer.py
@@ -10,24 +10,22 @@ class ParameterLayer(nn.Module):
     def __init__(self, input_size, k, d=2):
         super().__init__()
         self.linear = nn.Linear(input_size, k + 2*k*d + k + 3)
-        self.k = k
-        self.d = d
+        self.splits = [k, k*d, k*d, k, 3]
         self.reset_parameters()
 
     def reset_parameters(self):
         self.linear.reset_parameters()
 
     def forward(self, x, T=1):
+        # linear forward
         x = self.linear(x)
 
-        # unpack variables: [mix_probs, means, scales, corrs, endings]
-        d, k = self.d, self.k
-        splits = [k, k*d, k*d, k, 3]
-        mix_logits, means, scales, corrs, v_logits = x.split(splits, -1)
-        mix_logp = F.log_softmax(mix_logits, -1) # [n,t,k]
-        scales = torch.exp(scales) # [n,t,k,d]
-        corrs = torch.tanh(corrs) # [n,t,k]
-        v_logp = F.log_softmax(v_logits, -1) # [n,t,3]
+        # unpack variables & apply activation
+        mix_logits, means, scales, corrs, v_logits = x.split(self.splits, -1)
+        mix_logp = F.log_softmax(mix_logits, -1) # [...,k]
+        scales = torch.exp(scales) # [...,k*d]
+        corrs = torch.tanh(corrs) # [...,k]
+        v_logp = F.log_softmax(v_logits, -1) # [...,3]
 
         # reshape [...,k*d] -> [...,k,d]
         means = means.reshape(*means.shape[:-1], -1, 2)
